144a17d0010ef4e8c1d41db9d8a6cfae574160af
Tobias Heuer <tobias.heuer@gmx.net>
2020-03-31 06:30:58 +0200

    add install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+# Install Parkway
+cd partitioner/parkway
+./configure -DCMAKE_BUILD_TYPE=RELEASE
+make -j4
+
+# Install KaHyPar
+cd ..
+cd kahypar
+git submodule init
+git submodule update
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=RELEASE
+make KaHyPar
