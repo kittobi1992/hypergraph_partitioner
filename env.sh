afe45d6a9eb8426f9bc95982166842963c164497
Tobias Heuer <tobias.heuer@gmx.net>
2020-03-31 06:39:44 +0200

    add env bash script

---
+++ env.sh
@@ -0,0 +1,7 @@
+# Hypergraph Partitioner
+KAHYPAR="$HOME/hypergraph_partitioner/partitioner/build/kahypar/application/KaHyPar"
+MT_KAHYPAR="$HOME/mt-kahypar/build/mt-kahypar/application/KaHyPar"
+PARKWAY="$HOME/hypergraph_partitioner/partitioner/parkway/bin/parkway_driver"
+
+# Conversion Tool
+METIS_TO_PARKWAY_FORMAT="$HOME/mt-kahypar/build/tools/MetisToParkway"
