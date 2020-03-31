6eeff08dc01c646963f4a0f432017af18bfa0d8d
Tobias Heuer <tobias.heuer@gmx.net>
2020-03-31 12:55:01 +0200

    add mt-kahip

--- install.sh
+++ install.sh
@@ -13,3 +13,11 @@ cd build
 cmake .. -DCMAKE_BUILD_TYPE=RELEASE
 make KaHyPar
 make VerifyPartition
+
+# Install MT-KaHIP
+cd ../..
+cd KaHIP
+git checkout add_parallel_local_search
+git submodule init
+git submodule update
+scons program=kaffpa variant=optimized -j 4
