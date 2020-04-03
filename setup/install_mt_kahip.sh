3e113f3fbbffaee57bf97c160aa649d2c2294fea
Tobias Heuer <tobias.heuer@gmx.net>
2020-04-03 07:26:22 +0200

    add mt-kahip

--- setup/install_mt_kahip.sh
+++ setup/install_mt_kahip.sh
@@ -1,7 +1,9 @@
 # Install MT-KaHIP
-cd partitioner/KaHIP
+cd partitioner/mt-kahip
 git checkout add_parallel_local_search
 git submodule init
 git submodule update
+patch growt/data-structures/seqcircular.h < ../growt.patch
+patch SConscript < ../mt_kahip.patch
 scons program=kaffpa variant=optimized -j 4
 cd ../..
