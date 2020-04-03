cb690e4b22f26127420665294a7489d81a02d7d5
Tobias Heuer <tobias.heuer@gmx.net>
2020-04-03 09:31:06 +0200

    update kahypar install

--- setup/install_kahypar.sh
+++ setup/install_kahypar.sh
@@ -1,10 +1,13 @@
 # Install KaHyPar
+
 cd partitioner/kahypar
 git submodule init
 git submodule update
+
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=RELEASE
+cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DKAHYPAR_USE_MINIMAL_BOOST=ON
+make mini_boost -j4
 make KaHyPar
 make VerifyPartition
 make EvaluateMondriaanPartition
