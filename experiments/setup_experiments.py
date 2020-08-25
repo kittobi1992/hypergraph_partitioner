2cb86612f7f41ecba910fd82dca11b5e5e60390e
Nikolai Maas <nikolai.maas@student.kit.edu>
2020-08-25 12:11:58 +0200

    remove debug output

--- experiments/setup_experiments.py
+++ experiments/setup_experiments.py
@@ -175,7 +175,6 @@ def select_instance(partitioner, hg_name, k, separate, all_instances):
 
 def append_calls_from_metadata(config, data, partitioner_calls, partitioner, seed, result_dir, config_file, algorithm_name):
   all_instances = get_stripped_benchmark_instances(partitioner, config)
-  print("partitioner={}, instances={}".format(partitioner, all_instances))
   separate = bool(data["separate"])
 
   for hg_name, values in data["instances"].items():
