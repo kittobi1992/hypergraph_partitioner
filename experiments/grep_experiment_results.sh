cdb51269287d04cc25b0022c1b0b2052ac1e9099
Nikolai Maas <nikolai.maas@student.kit.edu>
2020-12-13 14:52:49 +0100

    adjust grep script

--- experiments/grep_experiment_results.sh
+++ experiments/grep_experiment_results.sh
@@ -1,10 +1,12 @@
 experiment_dir=$1
+epsilon=$2
 HEAD="algorithm,graph,timeout,seed,k,epsilon,num_threads,imbalance,totalPartitionTime,objective,km1,cut,failed"
 
-mkdir experimental_results
+mkdir -p experimental_results
 for result_folder in $experiment_dir/*_results;
 do
-  result_file="${result_folder/_results/.csv}"
+  result_file="${result_folder/_results}${epsilon}.csv"
+  echo $result_file
   rm -f $result_file
   echo "$HEAD" >> $result_file
   for instance_result_file in $result_folder/*.results;
@@ -14,5 +16,5 @@ do
   mv $result_file experimental_results/
 done
 
-tar -cvf experimental_results.tar experimental_results/*
-rm -rf experimental_results
+#tar -cvf experimental_results.tar experimental_results/*
+#rm -rf experimental_results
