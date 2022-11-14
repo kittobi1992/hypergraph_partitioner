85069ea9980cb19e0d908f5f6aba0a90acfb3706
Nikolai Maas <nikolai.maas@student.kit.edu>
2022-11-14 15:00:53 +0100

    measure timings in script

--- scripts/mt_kahypar_star.py
+++ scripts/mt_kahypar_star.py
@@ -75,6 +75,14 @@ num_separated = 0
 separated_weight = 0
 num_separated_in_community_detection = 0
 is_mesh_graph = False
+time_constr = 2147483647
+time_mem = 2147483647
+time_pre = 2147483647
+time_coarsen = 2147483647
+time_ip = 2147483647
+time_star = 2147483647
+time_ref = 2147483647
+time_post = 2147483647
 
 if mt_kahypar_proc.returncode == 0:
   # Extract metrics out of MT-KaHyPar output
@@ -89,20 +97,36 @@ if mt_kahypar_proc.returncode == 0:
       num_separated = int(s.split(" num_separated=")[1].split(" ")[0])
       separated_weight = int(s.split(" separated_weight=")[1].split(" ")[0])
       is_mesh_graph = bool(s.split(" is_mesh_graph=")[1].split(" ")[0])
-      sep_total_edges = bool(s.split(" sep_total_edges=")[1].split(" ")[0])
-      sep_intern_edge_fraction = bool(s.split(" sep_intern_edge_fraction=")[1].split(" ")[0])
-      total_density = bool(s.split(" total_density=")[1].split(" ")[0])
-      core_density = bool(s.split(" core_density=")[1].split(" ")[0])
-      sep_density = bool(s.split(" sep_density=")[1].split(" ")[0])
-      core_weight = bool(s.split(" core_weight=")[1].split(" ")[0])
-      stdev_factor = bool(s.split(" stdev_factor=")[1].split(" ")[0])
+      sep_total_edges = int(s.split(" sep_total_edges=")[1].split(" ")[0])
+      sep_intern_edge_fraction = float(s.split(" sep_intern_edge_fraction=")[1].split(" ")[0])
+      total_density = float(s.split(" total_density=")[1].split(" ")[0])
+      core_density = float(s.split(" core_density=")[1].split(" ")[0])
+      sep_density = float(s.split(" sep_density=")[1].split(" ")[0])
+      core_weight = float(s.split(" core_weight=")[1].split(" ")[0])
+      stdev_factor = float(s.split(" stdev_factor=")[1].split(" ")[0])
+    elif "+ Construct Hypergraph" in s:
+      time_constr = float(s.split("= ")[1].split(" ")[0])
+    elif "+ Memory Pool Allocation" in s:
+      time_mem = float(s.split("= ")[1].split(" ")[0])
+    elif "+ Preprocessing" in s:
+      time_pre = float(s.split("= ")[1].split(" ")[0])
+    elif "+ Coarsening" in s:
+      time_coarsen = float(s.split("= ")[1].split(" ")[0])
+    elif "+ Initial Partitioning" in s:
+      time_ip = float(s.split("= ")[1].split(" ")[0])
+    elif "+ Star Partitioning" in s:
+      time_star = float(s.split("= ")[1].split(" ")[0])
+    elif "+ Refinement" in s:
+      time_ref = float(s.split("= ")[1].split(" ")[0])
+    elif "+ Postprocessing" in s:
+      time_post = float(s.split("= ")[1].split(" ")[0])
 elif mt_kahypar_proc.returncode == -signal.SIGTERM:
   total_time = args.timelimit
   timeout = "yes"
 else:
   failed = "yes"
 
-# CSV format: algorithm,graph,timeout,seed,k,epsilon,num_threads,imbalance,totalPartitionTime,objective,km1,cut,failed,num_nodes,num_separated,separated_weight,is_mesh_graph,sep_total_edges,sep_intern_edge_fraction,total_density,core_density,sep_density,core_weight,stdev_factor
+# CSV format: algorithm,graph,timeout,seed,k,epsilon,num_threads,imbalance,totalPartitionTime,objective,km1,cut,failed,num_nodes,num_separated,separated_weight,is_mesh_graph,sep_total_edges,sep_intern_edge_fraction,total_density,core_density,sep_density,core_weight,stdev_factor,time_constr,time_mem,time_pre,time_coarsen,time_ip,time_star,time_ref,time_post
 print(algorithm,
       ntpath.basename(args.graph),
       timeout,
@@ -128,4 +152,12 @@ print(algorithm,
       sep_density,
       core_weight,
       stdev_factor,
+      time_constr,
+      time_mem,
+      time_pre,
+      time_coarsen,
+      time_ip,
+      time_star,
+      time_ref,
+      time_post,
       sep=",")
