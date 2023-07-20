4e3f5ceb1e6a75759ba4b6cc50fd90bb5688f3ed
Nikolai Maas <nikolai.maas@kit.edu>
2023-07-20 14:35:43 +0200

    update mt-metis installation url

--- setup/install_mt_metis.sh
+++ setup/install_mt_metis.sh
@@ -3,10 +3,10 @@ cd partitioner
 
 if [[ ! -d "mt-metis" ]]
 then
-  curl -o mt-metis.tar.gz http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/mt-metis-0.6.0.tar.gz
+  curl -o mt-metis.tar.gz https://dlasalle.github.io/mt-metis/releases/mt-metis-0.7.2.tar.gz
   tar -xvf mt-metis.tar.gz
   rm -f mt-metis.tar.gz
-  mv mt-metis-0.6.0 mt-metis
+  mv mt-metis-0.7.2 mt-metis
 fi
 
 cd mt-metis
@@ -14,3 +14,4 @@ mkdir build
 ./configure --prefix=build
 make -j4
 cd ../..
+
